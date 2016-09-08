fafedc865d4a9774b5eab2d22eb84358effac3fb
Daniel Kagemann <info@danielkagemannn.name>
2016-09-08 22:52:01 +0200

    setup page

--- js/ctrl.main.js
+++ js/ctrl.main.js
@@ -1,18 +1,26 @@
 /****************************************************************************************************************
  * controller
  ****************************************************************************************************************/
-angular.module('lawn').controller('MainController', function ($scope, $http, $location, $interval) {
+angular.module('lawn').controller('MainController', function ($scope, $http, $location, $interval){
 
       var vm = this, $handle = null, $checkEvery = 3;
       vm.simulation = true;
       vm.data = {};
       vm.action = "";
       vm.icon = "";
+      vm.config = {};
+
+      // everything initialized ?
+      $http.get('/data/init.json').then(function (response){
+         vm.config = response;
+      }, function (error){
+         $location.path("/setup");
+      });
 
       /**
        * show current state
        */
-      function $display() {
+      function $display(){
          // charging ?
          if (vm.data.batteryChargerState !== 'idle') {
             vm.action = "Aufladen";
@@ -44,21 +52,21 @@ angular.module('lawn').controller('MainController', function ($scope, $http, $lo
       /**
        * helper function for updating data
        */
-      function $getData() {
+      function $getData(){
          if (vm.simulation) {
             var states = ['home', 'idle', 'grass cutting', 'following wire', 'trapped recovery', 'outside wire'];
             vm.data.state = parseInt(Math.random() * states.length, 10);
             vm.data.state = states[vm.data.state];
-            vm.data.batteryChargerState = vm.data.state==='home' ? 'idle' : '';
+            vm.data.batteryChargerState = vm.data.state === 'home' ? 'idle' : '';
             vm.data.perc_batt = parseInt(Math.random() * 100, 10);
             $display()
          } else {
-            $http.get('server.php?q=info').then(function (response) {
+            $http.get('server.php?q=info').then(function (response){
                vm.data = response.data;
                console.info("state = " + vm.data.state);
 
                $display();
-            }, function (error) {
+            }, function (error){
                // we got an error.
                // todo: redirect to whatever the code is
                $location.path('/lost');
@@ -69,12 +77,12 @@ angular.module('lawn').controller('MainController', function ($scope, $http, $lo
       /**
        * on destroy of controller -> cleanup
        */
-      $scope.$on("destroy", function () {
+      $scope.$on("destroy", function (){
          $interval.cancel($handle);
       });
 
       // create interal
-      $handle = $interval(function () {
+      $handle = $interval(function (){
          $getData();
       }, $checkEvery * 1000);
 
@@ -84,7 +92,7 @@ angular.module('lawn').controller('MainController', function ($scope, $http, $lo
       /**
        * depending on load the batter has different color
        */
-      vm.batteryColor = function () {
+      vm.batteryColor = function (){
          if (angular.isDefined(vm.data.perc_batt)) {
             if (vm.data.perc_batt < 50) {
                return "low";
